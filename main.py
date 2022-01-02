d7cba6f76eda858cfa45feb662d219702c69b53a
Pedro Ostrschi <pedrotangerino@hotmail.com>
2022-01-02 16:42:45 -0300

    Aula 2 Tulpas ( a aula 1 foi apenas uma introdução, revisão)

--- main.py
+++ main.py
@@ -14,7 +14,11 @@ class Conta_Corrente:
 
 
 
-conta = Conta_Corrente("001")
-conta.deposita(1000)
 
-print(conta)
+julia = Conta_Corrente(13)
+pedro = Conta_Corrente(42)
+
+contas = (julia, pedro)
+julia.deposita(1000)
+for conta in contas:
+    print(conta)
